e5a69f8da769f60c02e87a84464b4f56715fece6
hazelbarcibal <hazelannmae.barcibal@gsfe.tupcavite.edu.ph>
2022-12-06 04:26:18 +0800

    alert pop-up & console.log upon submit

--- components/Passcodeforms.jsx
+++ components/Passcodeforms.jsx
@@ -1,17 +1,50 @@
 import styles from "./Passcodeforms.module.css";
+import { useForm } from "react-hook-form";
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Passcodeforms = () => {
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    toast.success('Form submitted!', {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
+  
+
+   
   return (
     <div className={styles.container}>
         <div className={styles.kahon}>
-          <form className={styles.Changepasscode}>
+          <form className={styles.Changepasscode} onSubmit={handleSubmit(onSubmit)}>
               <label htmlFor="oldpin">Old Pin:  </label><br></br>
-              <input id="oldpin"></input><br></br><br></br>
+              <input id="oldpin" {...register("oldpin", { required: true, maxLength: 25 })}></input><br></br><br></br>
               <label htmlFor="newpin">New Pin:  </label><br></br>
-              <input id="newpin"></input><br></br><br></br>
+              <input id="newpin" {...register("newpin", { required: true, maxLength: 25 })}></input><br></br><br></br>
               <label htmlFor="rnewpin">Re-Type New Pin:  </label><br></br>
-              <input id="rnewpin"></input><br></br><br></br><br></br>
-              <button  type="submit" id={styles.submitbtn}> Confirm</button>
+              <input id="rnewpin" {...register("rnewpin", { required: true, maxLength: 25 })}></input><br></br><br></br><br></br>
+              <button type="submit" id={styles.submitbtn}> Confirm</button>
+              <ToastContainer
+              position="top-center"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="colored"
+              />
           </form>
         </div>
         <div className={styles.Changepasscodepic}>
@@ -19,6 +52,6 @@ const Passcodeforms = () => {
         </div>
     </div>
   );
-};
+}
 
-export default Passcodeforms;
+
