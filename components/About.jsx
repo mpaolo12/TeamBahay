be53fa9180f88a32a8556a943d411f7b6b6cc1e2
emielaideligutan <emielaide.ligutan@gsfe.tupcavite.edu.ph>
2022-12-05 17:16:31 +0800

    update

--- components/About.jsx
+++ components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className={styles.container} id={'about'}>    
       <div className={styles.kahon}>
         <h1>ABOUT</h1>
-        <p>Monitoring security lock system is developed  by Team Bahay of BET-COET-NS-4B in the year of 2022.</p>
+        <p>Monitoring security lock system is developed  by Team Bahay of BET-COET-NS-4B in year 2022.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eleifend nisl id sapien porttitor, 
               nec efficitur ante viverra. Quisque sagittis blandit velit. Integer ac purus sit amet massa posuere elementum.
                Morbi iaculis orci sed eros vestibulum rhoncus. Cras a tortor non urna mollis sollicitudin sed ac velit. 
--- components/recordsitems.jsx
+++ components/recordsitems.jsx
@@ -1,38 +1,104 @@
 import styles from "./recordsitem.module.css";
 import Link from "next/link"
+import React from 'react'
+import { useTable } from 'react-table'
 
-const recordstable = () => {
+function Table() {
+
+  const columns = React.useMemo( () => [
+      {
+        Header: 'ID',
+        accessor: 'id', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Date',
+        accessor: 'date',
+      },
+      {
+        Header: 'Time',
+        accessor: 'time',
+      },
+      {
+        Header: 'Action',
+        accessor: 'action',
+      },
+   
+  ],
+  []
+  )
+
+  const data = React.useMemo( () => [
+     {
+          id: '1',
+          date: 'November 10, 2022',
+          time: '12:00PM',
+          action: <Link href='/Details' className={styles.detailbtn}>Details</Link>,
+     },
+     {
+          id: '2',
+          date: 'November 12, 2022',
+          time: '11:00PM',
+          action: <Link href='/Details' className={styles.detailbtn}>Details</Link>,
+    },
+    {
+          id: '3',
+          date: 'November 18, 2022',
+          time: '10:00PM',
+          action: <Link href='/Details' className={styles.detailbtn}>Details</Link>,
+    },
+        
+  ],
+  []
+  )
+
+  const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      rows,
+      prepareRow,
+  } = useTable ({ columns, data })
+  
   return (
-    <div className={styles.container}>
+
+  <div className={styles.container}>
       <div className={styles.records}>
-          <p> Please Select to view a Records</p>
-          
+        <p> Please Select to view a Records</p> 
       </div>
-      <div className={styles.app}>
-            <table>
-              <tbody>
-                <tr>
-                  <th>Date and Time</th>
-                  <th>Actions</th>
-                </tr>
-                <tr>
-                  <td>Nov 30, 2022 - 12:00pm</td>
-                  <td><button>Details</button></td>
-                </tr>
-                <tr>
-                  <td>Nov 30, 2022 - 12:00pm</td>
-                  <td><button>Details</button></td>
-                </tr>
-                <tr>
-                  <td>Nov 30, 2022 - 12:00pm</td>
-                  <td><button>Details</button></td>
-                </tr>
-              </tbody>
+      <table className={styles.table} {...getTableProps()}>
+          <thead>
+              { headerGroups.map(headerGroup => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map(column => (
 
-            </table>
-          </div>
-    </div>
-  );
-};
+                  <th
+                      {...column.getHeaderProps()} className={styles.th}>
+                      {column.render('Header')}
+                  </th>
+                  ))}
+                  </tr>
+              ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+              {rows.map(row => {
+                  prepareRow(row)
+                  return (
+                      <tr {...row.getRowProps()}>
+                          {row.cells.map(cell => {
+                              return (
+                                  <td
+                                      {...cell.getCellProps()} className={styles.td}>
+                                      {cell.render('Cell')}
+                                  </td>
+                              )
+                          })}
+                      </tr>
+                  )
+              })}
+          </tbody>
+     </table>
+  </div>
+  )
+}
 
-export default recordstable;
+export default Table
